f3c1b2b7e7fe598baf730efdc18e64f99edd63cb
alireza <alirezasalemi7@gmail.com>
2021-03-20 22:00:13 +0330

    Add route for welcome method in HelloController

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Header, Post } from '@nestjs/common';
+import { PersonDto } from './dto/person.dto';
+import { HelloService } from './hello.service';
 
 @Controller('hello')
-export class HelloController {}
+export class HelloController {
+
+    constructor(private readonly helloService : HelloService){}
+
+    @Post('welcome')
+    @Header('Content-Type', 'application/json')
+    async sayWelcome(@Body() personDto : PersonDto) : Promise<{data : String}> {
+        let msg = await this.helloService.welcome(personDto)
+        return {data : msg}
+    }
+}
