3c9365fddd921466b7858d6dc6f2bea55c30cd6d
alireza <alirezasalemi7@gmail.com>
2021-03-20 22:56:45 +0330

    Remove methods in app controler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,14 +4,5 @@ import { AppService } from './app.service';
 @Controller("/hello")
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get("/hi")
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get("/bye")
-  getBye(): string {
-    return this.appService.getHello();
-  }
+  
 }
