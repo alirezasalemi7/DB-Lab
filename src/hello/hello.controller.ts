27a86928f58d51583c17031089a96680a3346280
alireza <alirezasalemi7@gmail.com>
2021-03-20 22:06:42 +0330

    Add Get route for welcome method in HelloController

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Header, Post } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post, Query } from '@nestjs/common';
 import { PersonDto } from './dto/person.dto';
 import { HelloService } from './hello.service';
 
@@ -11,6 +11,12 @@ export class HelloController {
     @Header('Content-Type', 'application/json')
     async sayWelcome(@Body() personDto : PersonDto) : Promise<{data : String}> {
         let msg = await this.helloService.welcome(personDto)
+        return {data : msg}
+    }
+
+    @Get('welcome')
+    async sayWelcome2(@Query('name') name,@Query('year') year) : Promise<{data : String}> {
+        let msg = await this.helloService.welcome({'name':name,'year':year})
         return {data : msg}
     }
 }
